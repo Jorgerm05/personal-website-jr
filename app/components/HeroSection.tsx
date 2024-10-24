fce7849fdc01d7253006f42555bc249dab4b8a6a
Jorge Ramos <jorgeramosma@hotmail.com>
2024-10-24 14:48:46 -0500

    My Personal Website JR

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -15,8 +15,9 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I&apos;m{" "}</span>
+           <h1 className="text-blue-600 mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+  <span className="text-transparent bg-clip-text bg-gradient-to-r from-sky-400 to-blue-600">Hello, I&apos;m{" "}</span>
+ 
             <br></br>
             <TypeAnimation
                 sequence={[
